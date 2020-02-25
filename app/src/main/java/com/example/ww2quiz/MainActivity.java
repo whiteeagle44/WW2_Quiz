4156fb754675917dff18cbd4d5e3a876d0a4fe43
kalucky0 <kalucki23@gmail.com>
2020-02-25 10:13:30 +0100

    Updated to AndroidX

--- app/src/main/java/com/example/ww2quiz/MainActivity.java
+++ app/src/main/java/com/example/ww2quiz/MainActivity.java
@@ -2,7 +2,7 @@ package com.example.ww2quiz;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
